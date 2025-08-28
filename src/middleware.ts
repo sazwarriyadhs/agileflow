aec98e0d42bcb2c04cce8ad075ceb598c68876d2
Taman Samping <initamansamping@gmail.com>
2025-08-28 06:56:48 +0000

    404 This page could not be found.

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/', '/(en|id)/:path*']
 };
