6aa03be3cb64fa9212d92e4246a143ff0151439f
Taman Samping <initamansamping@gmail.com>
2025-08-28 08:11:32 +0000

    ukuran logo.png 200 X 47 px

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -36,7 +36,6 @@ import {
   SidebarProvider,
   SidebarTrigger,
   SidebarFooter,
-  SidebarInset,
 } from '@/components/ui/sidebar';
 import { Button } from '@/components/ui/button';
 
@@ -65,7 +64,7 @@ export default function DashboardLayout({
         <Sidebar>
           <SidebarHeader className="p-4">
             <div className="flex items-center gap-2 px-6 py-4">
-              <Image src="/images/logo.png" width={32} height={32} alt="AgileFlow logo" className="h-8 w-8" />
+              <Image src="/images/logo.png" width={200} height={47} alt="AgileFlow logo" />
             </div>
           </SidebarHeader>
           <SidebarContent>
